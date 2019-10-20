c688849a0630dd4f8aea20dc3b7510698879a181
wwwkuba14 <szwajkajakub@onet.pl>
2019-10-20 22:41:41 +0200

    home work

--- ant.py
+++ ant.py
@@ -1,6 +1,7 @@
 
 import utils
 import random
+# import numpy as np
 
 class Ant( ):
     def __init__ (self, nodes):
@@ -8,31 +9,34 @@ class Ant( ):
         self.all_loc_visited = False
         self.distance_travelled = 0.0
         self.route = nodes
-        self.location = self.route[0]
+        self.current_location = self.route[0]
         self.next_location = self.route[0]
         
     def run(self):
         self.not_visited_locations = self.route
         print( self.not_visited_locations )
+
         while self.not_visited_locations:
-           # while set(self.next_location) & set(self.location):
+           # while set(self.next_location) & set(self.current_location):
             self.next_location = self.find_path( )
-            self.go_to(self.location , self.next_location)
+            self.go_to(self.current_location , self.next_location)
 
         self.all_loc_visited = True
+
         print( self.distance_travelled )
 
     def find_path(self):
-        return random.choice( self.not_visited_locations )
+        return random.choice( self.not_visited_locations)
 
 
-    def go_to(self, location , next_location):
+    def go_to(self, current_location , next_location):
         self.distance_travelled = self.distance_travelled + utils.count_distance( location , next_location)
-        # print ( location )
+        # print ( current_location )
         # print ( next_location )
         # print ( self.not_visited_locations )
-        if location in self.not_visited_locations:
-            self.not_visited_locations.remove(location)
-        self.location = next_location
+        if current_location in self.not_visited_locations:
+            self.not_visited_locations.remove(current_location)
+        self.current_location = next_location
 
+    
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import utils 
+import numpy
 from ant import Ant
 
 # punkt startowy ze wzgledu na numer indeksu 3 – B
@@ -13,9 +14,45 @@ nodes = [[1,2],[3,1],[3,6],[6,7],[5,2]]
 
 alfa = 1 
 beta = 1 
+transition_probabilities=[]
+tau = []
+ants = []
 
-# dist = utils.count_distance( A , B )
 
-a = Ant(nodes)
-a.run( )
+for city in nodes:
+    for city2 in nodes:
+        if city != city2:        
+            single_route = [city , city2]
+            if single_route not in tau:
+                single_route[0], single_route[1] = single_route[1] , single_route[0]
+                if single_route not in tau:
+                    tau.append(single_route)
 
+print("===============================")
+
+for city in tau:
+    print (city)
+
+
+
+
+
+
+for i in range(10):
+    ants.append( Ant( nodes ))
+
+
+for ant in ants:
+    current_city = ant.current_location
+    
+    for city in ant.route:
+        denominator = 0
+        numerator = (pow(tau[current_city][city],alpha)*(eta[current_city][city], beta))
+        for city in ant.route:
+            denominator += (pow(tau[current_city][city],alpha)*(eta[current_city][city], beta))
+        
+        p_ij = numerator/float(denominator)
+        transition_probabilities.append(p_ij)
+    
+    next_city = numpy.random.choice(ant.route, 1, transition_probabilities)  
+    print(next_city)
